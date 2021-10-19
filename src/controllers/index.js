2815bf034c6bd56dc3aa748ae8c4b83f0372e95a
Your Name <you@example.com>
2021-10-20 01:08:36 +0700

    first commit

---
+++ src/controllers/index.js
@@ -0,0 +1,13 @@
+module.exports.authController = require('./auth.controller');
+module.exports.userController = require('./user.controller');
+module.exports.studentController = require('./student.controller');
+module.exports.lopController = require('./lop.controller');
+module.exports.peopleController = require('./people.controller');
+module.exports.professionalController = require('./professional.controller');
+module.exports.worksforController = require('./worksfor.controller');
+module.exports.companiesController = require('./companies.controller');
+
+
+
+
+
---
+++ src/validations/index.js
@@ -0,0 +1,14 @@
+module.exports.authValidation = require('./auth.validation');
+module.exports.userValidation = require('./user.validation');
+module.exports.studentValidation = require('./student.validation');
+module.exports.lopValidation = require('./lop.validation');
+module.exports.peopleValidation = require('./people.validation');
+module.exports.professionalValidation = require('./professional.validation');
+module.exports.worksforValidation = require('./worksfor.validation');
+module.exports.companiesValidation = require('./companies.validation');
+
+
+
+
+
+
